2b5b04a15d836f8f59a4c2ac987271991bf151ad
Alvin Jing <jingqi.hanzhong@gmail.com>
2015-06-24 18:03:31 -0300

    Added scope in todo_item and two functions in todo_list

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,4 +1,7 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
   validates :content, presence: true
+
+  scope :complete, -> {where("completed_at is not null")}
+  scope :incomplete, -> {where(completed_at: nil)}
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -3,4 +3,12 @@ class TodoList < ActiveRecord::Base
 
 	validates :title, presence: true, length: { minimum: 2 }
 	validates :description, presence: true
+
+	def has_completed_items? 
+		todo_items.complete.size > 0
+	end
+
+	def has_incompleted_items?
+		todo_items.incomplete.size > 0
+	end
 end
